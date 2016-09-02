eed1a123a5ea84c1061d0ae220a72bd29ae908d2
Robert Clifford Jordan <rcjordan43@students.tntech.edu>
2016-09-01 20:06:13 -0500

    Attempt 1 at Converting ReadFile to a class

--- ReadFile.h
+++ ReadFile.h
@@ -8,16 +8,19 @@ using namespace std;
 
 struct ReadFile
 {
-   ifstream input_file;
-   bool _eof;
-   bool closed;
+	private:
+		ifstream input_file;
+		bool _eof;
+		bool closed;
+	public:
+		ReadFile(const char* file_name);
+		~ReadFile();
+		String* readLine();
+		bool eof();
+		void close();
 };
-//inserted comment
 
-ReadFile* createReadFile(const char* file_name);
-void destroyReadFile(ReadFile* rf);
-String* readLine(ReadFile* rf);
-bool eof(ReadFile* rf);
-void close(ReadFile* rf);
+
+
 
 #endif
